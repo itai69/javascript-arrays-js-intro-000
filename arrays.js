c4d04968952cc908b74156b1683cb6867cb1a1aa
itai rosenberg <djitai69@gmail.com>
2019-10-19 15:35:42 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = [snickers, hundred grand, kitkat, skittles];
+
+function addElementToBeginningOfArray(array, element){
+  const array = [1]
+  addElementToBeginningOfArray([1],'foo')
+  array.unshift('foo')
+  return array
+}
+
+
+//function destructivelyAddElementToBeginningOfArray
